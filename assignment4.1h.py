e5e92853dcac2b8f68f68a6a82db1a6588a4666a
kccastrosfu <kccastro@sfu.ca>
2016-06-23 12:13:32 -0700

    Add files via upload

---
+++ assignment4.1h.py
@@ -0,0 +1,16 @@
+#CMPT 120 
+######################################################
+##This is a test question. Define a function called sum2 which takes two integer inputs (parameters) and returns the sum of these values.
+##As an example, the following code fragment:
+##first = 10
+##second = 20
+##theSum = sum2(first,second)
+##print (theSum)
+##should produce the output:
+##30
+#######################################################
+def sum2(a,b):
+    result= a+b
+    return result
+
+print sum2(10,20)
